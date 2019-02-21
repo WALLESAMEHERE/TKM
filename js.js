aa6404c6bedd0d4790dda53774b31c093a3d97bb
KANTERSTRAJK <KANTERSTRAJK>
2019-02-21 23:29:43 +0100

    added some new styles and upper lower letter function

--- js.js
+++ js.js
@@ -5,6 +5,9 @@ const btnDelete = document.getElementById('btnDelete');
 const counteChars = document.getElementById('counteChars');
 const showRes = document.getElementById('result');
 const numberOfChar = document.getElementById('numberOfChar');
+const toLowerLetter = document.getElementById('toLowerLetter');
+const toUpperLetter = document.getElementById('toUpperLetter');
+
 
 
 btnShow.addEventListener('click',function(){
@@ -33,10 +36,25 @@ btnDelete.addEventListener('click', ()=>{
   numberOfChar.innerHTML = 0;
 })
 
+toLowerLetter.addEventListener('click', ()=>{
+  let valueArea = document.getElementById('area').value;
+  showRes.innerHTML = valueArea.toLowerCase();
+})
+
+toUpperLetter.addEventListener('click', ()=>{
+  let valueArea = document.getElementById('area').value;
+  showRes.innerHTML = valueArea.toUpperCase();
+})
+
 counteChars.addEventListener('click', ()=>{
   let valueArea = document.getElementById('area').value;
   numberOfChar.innerHTML = valueArea.length;
 })
+
+
+
+
+
 
 
 
